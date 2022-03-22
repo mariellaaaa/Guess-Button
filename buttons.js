e75bbe18acd5d0b42cb9684b700d2f61c4a38675
mariellaaaa <101421492+mariellaaaa@users.noreply.github.com>
2022-03-22 22:56:24 +0200

    Update buttons.js

--- buttons.js
+++ buttons.js
@@ -1,32 +1,23 @@
 <script>
-  var random;
   function buttonValue() {
-    let values = $(`#values`).val();
-    random = Math.floor(Math.random() * values) + 1;
+    let values = document.getElementById('values').value;
+    let random = Math.ceil(Math.random() * values);
     if (values < 2) {
       alert("The number of participating buttons cannot be less than 2!")
       window.location.reload();
     }
     for(let i = 1; i <= values; i++) {
-      $(`#list`).append(`
-      <div class="card" id="`+ i +`">
-        <div class="card-body">
-          <h5>Maybe that's the winning button</h5>
-        </div>
-        <div class="card-footer text-muted">
-          <a href="#" class="btn btn-info" onclick="return winningButton(`+ i +`);" >Try</a>
-        </div>
-      </div>
-    `)
-    }
-    return false;
-  }
-  function winningButton(idValues) {
-    if(random == idValues) {
-      alert("Congrats!You have chosen the winning button!")
-      window.location.reload();
-    } else {
-      alert("Sorry, the selected button is not the winner. Please, try again!")
+      let btn = document.createElement('button');
+      btn.innerHTML = '<button type="button" class="btn btn-success">Try</button>';
+      document.getElementById('list').appendChild(btn);
+      btn.onclick = function() {
+        if(random == i) {
+          alert("Congrats!You have chosen the winning button!")
+          window.location.reload();
+        } else {
+          alert("Sorry, the selected button is not the winner. Please, try again!")
+        }
+      }
     }
     return false;
   }
